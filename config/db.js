9aabfcbf00215d38dfcd23ea359f2fd0e0ffb808
RitikBishwas <134728719+RitikBishwas@users.noreply.github.com>
2024-07-20 23:40:48 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -3,12 +3,10 @@ import colors from "colors";
 const connectDB = async () => {
   try{
     const conn=await mongoose.connect(process.env.MONGO_URL);
-    console.log('connected to ${conn.connection.host}');
+    console.log('connected to mongoDB');
   }
   catch(error){console.log(`error in mongodb`);
 }
-  //await mongoose.connect("mongodb+srv://bishwasritik07:wnFzKjJVInHjfhHG@cluster0.4bxa5qp.mongodb.net/ecommerceapp").then(()=>{console.log("connected");
-//});
 };
 
 export default connectDB;
